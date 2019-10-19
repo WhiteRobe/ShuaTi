001a4221956db8578c01077caf81a8b96f164d0e
W.R-d <sirorobe@outlook.com>
2019-10-19 13:45:58 +0800

    顺时针打印矩阵
    Signed-off-by: W.R-d  <sirorobe@outlook.com>

---
+++ newcoder/_test_/trick/test_顺时针打印矩阵.py
@@ -0,0 +1,36 @@
+import unittest
+from newcoder.trick import 顺时针打印矩阵 as tT
+
+
+class MyTestCase(unittest.TestCase):
+    def test_normal(self):
+        s = tT.Solution()
+        self.assertEqual(s.printMatrix([[1, 2, 3],
+                                        [4, 5, 6],
+                                        [7, 8, 9],
+                                        [10, 11, 12]], should_print=False),
+                         [1, 2, 3, 6, 9, 12, 11, 10, 7, 4, 5, 8])
+
+    def test_normal_square(self):
+        s = tT.Solution()
+        self.assertEqual(s.printMatrix([[1, 2, 3, 4],
+                                        [5, 6, 7, 8],
+                                        [9, 10, 11, 12],
+                                        [13, 14, 15, 16]], should_print=False),
+                         [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10])
+
+    def test_empty(self):
+        s = tT.Solution()
+        self.assertEqual(s.printMatrix([[]], should_print=False), [])
+
+    def test_one_row(self):
+        s = tT.Solution()
+        self.assertEqual(s.printMatrix([[1, 2, 3, 4]], should_print=False), [1, 2, 3, 4])
+
+    def test_one_col(self):
+        s = tT.Solution()
+        self.assertEqual(s.printMatrix([[1], [2], [3]], should_print=False), [1, 2, 3])
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ newcoder/trick/顺时针打印矩阵.py
@@ -0,0 +1,90 @@
+"""
+题目来源:
+    牛客网
+    @See https://www.nowcoder.com/practice/9b4c81a02cd34f76be2659fa0d54342a
+    题目序号 剑指Offer 20
+题目描述:
+    输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字，
+思路:
+    把矩阵想象成若干个圈，每次打印一个圈
+样例输入:
+    1 2 3 4
+    5 6 7 8
+    9 10 11 12
+    13 14 15 16
+样例输出:
+    1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10
+"""
+
+
+class Solution:
+    # matrix类型为二维列表，需要返回列表
+    def printMatrix(self, matrix, should_print=True):
+        # write code here
+        rows, cols = len(matrix), len(matrix[0])
+        top, right, bottom, left = -1, cols, rows, -1  # 给定界限
+        results, c, c_d = [], [0, 0], 0  # 结果集 当前坐标 当前方向
+        # directions = [0, 1, 2, 3]  # 右 下 左 上
+
+        while len(results) < rows * cols:
+            delta_x = 1 if c_d == 1 else (-1 if c_d == 3 else 0)
+            delta_y = 1 if c_d == 0 else (-1 if c_d == 2 else 0)
+
+            fr, to = self.select_from_to(c_d, top, right, bottom, left)
+
+            for _ in range(fr+delta_x+delta_y, to, delta_x+delta_y):
+                # index = 1 if abs(delta_x) else 0
+                c[0 if abs(delta_x) else 1] = _  # 上下移动时
+                print(delta_x, delta_y, fr, to, c)
+                results.append(matrix[c[0]][c[1]])
+
+            c_d, top, right, bottom, left = self.shrink(c_d, top, right, bottom, left)
+
+        if should_print:
+            self.out_print(results)
+
+        return results
+
+    def select_from_to(self, direction, top, right, bottom, left):
+        """
+        选择正确的遍历方向
+        :param direction:
+        :param top:
+        :param right:
+        :param bottom:
+        :param left:
+        :return:
+        """
+        if direction == 0:
+            return left, right
+        elif direction == 1:
+            return top, bottom
+        elif direction == 2:
+            return right, left
+        elif direction == 3:
+            return bottom, top
+
+    def shrink(self, direction, top, right, bottom, left):
+        """
+        尺度放缩
+        :param direction: 当前方向
+        :param top:
+        :param right:
+        :param bottom:
+        :param left:
+        :return:
+        """
+        if direction == 0:
+            top += 1
+        elif direction == 1:
+            right -= 1
+        elif direction == 2:
+            bottom -= 1
+        elif direction == 3:
+            left += 1
+
+        return (direction + 1) % 4, top, right, bottom, left
+
+    def out_print(self, arr):
+        for i in arr:
+            print(i)

ad7c0b02e32e50b0aa93df077ebabfdeaf06a6e2
W.R-d <sirorobe@outlook.com>
2019-10-24 12:35:34 +0800

    修正上一次提交忘记添加stack.py改动的错误
    Signed-off-by: W.R-d  <sirorobe@outlook.com>

--- structure/stack.py
+++ structure/stack.py
@@ -3,29 +3,62 @@ from structure.list_node import DuelNode
 
 class Stack:
     def __init__(self):
-        self.top = None
-        self.size = 0
+        self.m_top = None
+        self.m_size = 0
 
     def push(self, node):
         n = DuelNode(node)
-        n.prev = self.top
-        self.top = n
-        self.size += 1
+        n.prev = self.m_top
+        self.m_top = n
+        self.m_size += 1
 
     def pop(self):
-        popout = self.top
-        self.top = popout.prev if popout is not None else None
-        self.size = max(0, self.size-1)
-        return popout.val
+        popout = self.m_top
+        self.m_top = popout.prev if popout is not None else None
+        self.m_size = max(0, self.m_size-1)
+        return popout.val if popout is not None else None
 
     def clean(self):
-        self.top = None
-        self.size = 0
+        self.m_top = None
+        self.m_size = 0
+
+    def size(self):
+        return self.m_size
+
+    def top(self):
+        return self.m_top.val if self.m_top is not None else None
 
     def empty(self):
-        return self.size == 0
+        return self.m_size == 0
 
     def pop_to(self, stack):
-        while self.size > 0:
+        while self.m_size > 0:
             stack.push(self.pop())
 
+
+class ListStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, node):
+        self.stack.append(node)
+
+    def pop(self):
+        return self.stack.pop(-1) if self.size() > 0 else None
+
+    def size(self):
+        return len(self.stack)
+
+    def top(self):
+        return self.stack[-1] if self.size() > 0 else None
+
+    def clean(self):
+        self.stack = []
+
+    def empty(self):
+        return self.size() == 0
+
+    def pop_to(self, stack):
+        while self.size() > 0:
+            stack.push(self.pop())
